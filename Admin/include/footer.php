0773dcb2e7dcad84e3a9ff6d0fae53b6abf1c158
steven opio <stevenviko@gmail.com>
2021-01-26 23:33:54 +0300

    Update footer.php

--- Admin/include/footer.php
+++ Admin/include/footer.php
@@ -1,7 +1,7 @@
 	<div class="footer">
 		<div class="container">
 			 
-<?php $currentY= date( 'Y', time () );?>
+<?php $currentY= date('Y', time () );?>
 			<b class="copyright">&copy; <?php echo $currentY; ?> GreciaJewellery Sote </b> All Rights Reserved.
 		</div>
 	</div>
